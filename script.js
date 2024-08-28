b52e25aa0f3c0d89b6662ce745d9eca27f9c24dd
Vitor Gabriel Pinheiro Gomes <141376701+Vitorgpg@users.noreply.github.com>
2024-08-28 20:28:17 -0300

    vdd

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+Var palavravariavel-?
+funcaoes{prompt(""): string
+         parseint(""):number
+         parsefloat("")number
+         console.log(""):void}
+___________________________________
++ soma ou concatena
+- subtrai
+* multiplica
+/ divide
+___________________________________
+
+condicoes - retorna um boolean um true-false
+
+if(_________________ ________ _________________)
+   var ou texto-num           var ou texto-num
+
+== igual//comparação
+> maior
+> maior igual
+< menor 
+< menor igual
+!= diferente
+*/
+
+var anoAtual = 2024;
+var anoNascimento = parseInt(prompt("Digite seu ano de nascimento:"));
+var idade = anoAtual - anoNascimento;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade ?:")
+    console.log("Você é obrigado a votar!")
+}else{
+    console.log("você é menor de idade no Brasil!");
+}
+
+
+// Manipuland o IF
+console.log(" Usando apenas o > (idade > 17 )");
+if ( idade > 17) { 
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
+
+console.log("Invertendo o resultado da idade < 18 ");
+if ( idade < 18) {
+    console.log("Menor de idade");
+} else {
+    console.log("Maior de idade");
+}
